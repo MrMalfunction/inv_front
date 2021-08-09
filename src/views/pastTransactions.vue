205076e045475b9ae4dd153d09aa80be741390f3
Amol Bohora <007bohora@gmail.com>
2021-08-10 02:08:34 +0530

    80% done

--- src/views/pastTransactions.vue
+++ src/views/pastTransactions.vue
@@ -5,6 +5,42 @@
       <h3 class="md-title" style="flex: 1; text-align: center">Welcome to {{ this.shopName }}</h3>
     </md-toolbar>
     Welcome {{ username }}<br>
+    <div class="Form">
+      <form novalidate @submit.prevent="">
+        <div class=" mainArea md-small-size-100 md-elevation-15">
+          <span class="md-display-2">View Past Transactions</span><br>
+          <div class="date_picker">
+            <md-datepicker v-model="fromDate" md-immediately>
+              <label>From Date</label>
+            </md-datepicker>
+          </div>
+          <div class="date_picker">
+            <md-datepicker v-model="toDate" md-immediately>
+              <label>To Date</label>
+            </md-datepicker>
+          </div>
+          <div class="md-layout-item">
+            <md-field>
+              <md-select v-model="type" placeholder="Type of Transaction">
+                <md-option value="IN" aria-selected="true">IN</md-option>
+                <md-option value="OUT">OUT</md-option>
+              </md-select>
+              <span class="md-helper-text">Default Value IN</span>
+            </md-field>
+          </div>
+          <md-autocomplete v-model="merchant" :md-options="merchantList" :class="{ 'md-invalid': merchantNameCheck }" :md-open-on-focus="false" :md-fuzzy-search="true">
+            <label>Merchant Name</label>
+            <template slot="md-autocomplete-item" slot-scope="{ item, term }">
+              <md-highlight-text :md-term="term">{{ item }}</md-highlight-text>
+            </template>
+            <span class="md-helper-text">Optional</span>
+          </md-autocomplete>
+          <div>
+            <md-button type="submit" class="md-accent" @click="submit">Submit</md-button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -17,15 +53,21 @@ const cookieExpiry = "100s";
 
 export default {
   name: "pastTransactions",
-  components: {
-  },
-  computed: {
-  },
+  components: {},
+  computed: {},
   data() {
+    let now = new Date()
+    let past = (d => new Date(d.setDate(d.getDate() - 7)))(new Date)
     return {
       username: null,
       shopName: null,
-      merchantNameCheck: false,
+      merchantName: null,
+      merchantList: [],
+      type: "",
+      fromDate: Number(now),
+      toDate: Number(past),
+      tableData: null,
+      lastKey: null
 
     }
   },
@@ -70,7 +112,40 @@ export default {
         }
       }
     },
-
+    listChecker() {
+      let local_data = localStorage.getItem("Merchant");
+      console.log(local_data)
+      if (local_data === null) {
+        var data = JSON.stringify({
+          "shopid": Vue.$cookies.get("shopid"),
+          "type": "Merchant",
+          "selection": "Retrieve",
+          "len": 0
+        })
+        var config = {
+          method: 'post',
+          url: 'https://inv.amolbohora.com/auto',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: data
+        };
+        var that = this;
+        axios(config)
+            .then(function (response) {
+              console.log(response.data['Data'])
+              that.merchantList = response.data['Data']
+              localStorage.setItem("Merchant", JSON.stringify(that.merchantList))
+            })
+            .catch(function (error) {
+              that.showSnackbar = true;
+              that.errorMessage = error.response.data['Message'];
+              console.log(error);
+            });
+      } else {
+        this.merchantList = JSON.parse(localStorage.getItem("Merchant"))
+      }
+    },
     roleCheck() {
       if (!Vue.$cookies.isKey("Roles")) {
         Vue.$cookies.remove("cookie")
@@ -93,6 +168,7 @@ export default {
   beforeMount() {
     this.CC();
     this.roleCheck();
+    this.listChecker();
     this.autoOn = localStorage.getItem("auto") === 'true';
     this.active = localStorage.getItem("auto") === 'true';
   }
@@ -101,4 +177,21 @@ export default {
 
 <style scoped>
 
+.date_picker {
+  display: inline-flex;
+  min-width: 25vw;
+}
+
+.mainArea {
+  min-width: 50vw;
+  margin: 20px 0 0 0;
+  padding: 20px;
+  display: inline-block;
+  vertical-align: center;
+  border-radius: 10px;
+}
+
+.Form {
+  padding: 10px;
+}
 </style>
